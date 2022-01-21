5e199f246aa2f0687c25f4154443052fc632dc33
PHermanov <germanovpavel@gmail.com>
2022-01-21 17:33:24 +0200

    add swagger config for auth

--- SeenLive.Web/Startup.cs
+++ SeenLive.Web/Startup.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 using FluentValidation.AspNetCore;
@@ -36,40 +37,64 @@ namespace SeenLive.Api
             // Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SeenLiveDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
 
             services.AddDbContext<AppDbContext>(options =>
-                    options.UseSqlServer(Configuration["DefaultConnectionString"], 
+                    options.UseSqlServer(Configuration["DefaultConnectionString"],
                         m => m.MigrationsAssembly("SeenLive.Api")));
 
             // For Identity  
-            services.AddIdentity<User, IdentityRole>()  
-                .AddEntityFrameworkStores<AppDbContext>()  
-                .AddDefaultTokenProviders();  
-  
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
             // Adding Authentication  
-            services.AddAuthentication(options =>  
-                {  
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;  
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;  
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;  
-                })  
-  
-                // Adding Jwt Bearer  
-                .AddJwtBearer(options =>  
-                {  
-                    options.SaveToken = true;  
-                    options.RequireHttpsMetadata = false;  
-                    options.TokenValidationParameters = new TokenValidationParameters()  
-                    {  
-                        ValidateIssuer = true,  
-                        ValidateAudience = true,  
-                        ValidAudience = Configuration["JWT:ValidAudience"],  
-                        ValidIssuer = Configuration["JWT:ValidIssuer"],  
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))  
-                    };  
-                });  
-            
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidAudience = Configuration["JWT:ValidAudience"],
+                        ValidIssuer = Configuration["JWT:ValidIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                    };
+                });
+
             services.AddSwaggerGen(swagger =>
             {
                 swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "SeenLive API" });
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
             });
 
             services.AddMediatR(Assembly.GetAssembly(typeof(HandlerBase)));
@@ -98,8 +123,8 @@ namespace SeenLive.Api
 
             app.UseRouting();
 
-            app.UseAuthentication();  
-            app.UseAuthorization();  
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
